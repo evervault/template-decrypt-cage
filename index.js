3f9739d68ce8f7c8e99bc16175b97d487c83b421
Shane Curran <shane@evervault.com>
2020-10-08 10:39:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const got = require('got');
 
-exports.handler = async (data) => {
-    const { url, options } = data;
-    const { body } = await got(url, options);
-
-    return body;
-};
+exports.handler = async (data) => data;
